3d9730a5bfe4513a054168cc588d798add48444f
Kirill Koval <brcyrill2@gmail.com>
2023-05-02 17:43:19 +0300

    added Flight model

---
+++ model/flight.py
@@ -0,0 +1,7 @@
+class Flight:
+    def __init__(self, departure_place, departure_datetime, arrival_place, arrival_datetime, price):
+        self.departure_place = departure_place
+        self.departure_datetime = departure_datetime
+        self.arrival_place = arrival_place
+        self.arrival_datetime = arrival_datetime
+        self.price = price
